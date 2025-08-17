11cbe92abb281deec43d313cd69e8fd2b12d94c6
Brian Reece <bdreece@mtu.edu>
2025-08-17 13:15:02 -0400

    ci: add logging to download artifact script

--- .github/download-artifacts.cjs
+++ .github/download-artifacts.cjs
@@ -15,10 +15,14 @@ async function downloadArtifacts(github, { outdir }) {
     run_id: this.payload.workflow_run.id,
   });
 
+  console.debug("listed %d total artifacts", artifactResult.data.total_count);
+
   // Find `dist` artifact
   const match = artifactResult.data.artifacts.find(
     (artifact) => artifact.name === "dist"
   );
+
+  console.debug("found target artifact: %s", match.url);
 
   // Download `dist` artifact
   const downloadResult = await github.rest.actions.downloadArtifact({
@@ -28,15 +32,19 @@ async function downloadArtifacts(github, { outdir }) {
     archive_format: "zip",
   });
 
+  const data = Buffer.from(downloadResult.data);
+  console.debug("downloaded artifact: %d bytes", data.length);
+
   // Make target directory
   if (!fs.existsSync(outdir)) {
     fs.mkdirSync(outdir);
+    console.debug("created directory: %s", outdir);
   }
 
   // Write artifact to target directory
   const filename = path.join(outdir, "dist.zip");
-  const data = Buffer.from(downloadResult.data);
   fs.writeFileSync(filename, data);
+  console.info("done! wrote file: %s", filename);
 }
 
 module.exports = downloadArtifacts;
